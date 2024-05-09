9cb7b91b72d8c5f5b6bbee38f5114ad45a2d8ee4
mattgarbecki <mattgarbecki@gmail.com>
2024-05-09 17:58:47 -0400

    Added initial version of the auto spell checker with the preprocessed file and spelling corrections

---
+++ src/configs/find_name_corrections.py
@@ -0,0 +1,24 @@
+# Setting up a dictionary with all the configs for the find_name_corrections DAG
+#input column names
+CONSTANTS = {
+    "division": "Division",
+    "play_round": "Round",
+    "first_team": "Team A",
+    "second_team": "Team B",
+    "first_team_first_player": "Team A - Player 1",
+    "first_team_second_player":  "Team A - Player 2",
+    "second_team_first_player":  "Team B - Player 1",
+    "second_team_second_player": "Team B - Player 2",
+}
+CFGS = {
+    "data_input_path": "Tourney Results/Manual Downloads/",
+    "data_output_path": "Tourney Results/Preprocessed/",
+    "spelling_output_path": "Players/",
+    "minimum_misspelling_confidence": 80,
+    "interactive_spelling_correction": False,
+    "str_cols_to_check": [
+        CONSTANTS['division'],  # This means division names
+        # CONSTANTS['first_team'],  # This means team names
+        CONSTANTS['first_team_first_player'],  # This means player names
+    ],
+}
